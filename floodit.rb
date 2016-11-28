cd62e0b141eaa7bc9aac4b1752fba369fe1766da
John Ayad <jayad1@sheffield.ac.uk>
2016-11-28 03:17:35 +0000

    Work in progress
    Add game play window
    TODO:
    -Fix up recursive function
    -Update counter on each turn

--- floodit.rb
+++ floodit.rb
@@ -1,9 +1,11 @@
 require "console_splash"
+require "colorize"
 
 #Global Variables
 $high_score = -1
 $width_main = 14
-$heigh_main = 9
+$height_main = 9
+$counter_turns = 0
 
 def get_board(width, height)
   # beta implementation
@@ -54,6 +56,7 @@ def main_menu()
     status = gets.chomp
     
     if (status.downcase == "s")
+        counter_turns = 0
         start_game()
     elsif (status.downcase == "c")
         settings()
@@ -66,8 +69,68 @@ end
 
 def start_game()
     board = get_board($width_main, $height_main)
+    game_play(board)
 end
     
+def game_play(board)
+    system ("clear")
+    for i in 0...board.size
+        for j in 0...board[i].size
+            print "  ".colorize(:background => board[i][j])
+        end
+        puts
+    end
+    puts "Turns: #$counter"
+    percentage = completed_percent(board)
+    puts "Current completion: #{percentage}%"
+    puts "Choose a colour: "
+    colour_input = gets.chomp
+    if (colour_input.downcase == "r")
+        update_board(board, ":red", board[i][j], 0, 0)
+        game_play(board)
+    elsif (colour_input.downcase == "b")
+        update_board(board, ":blue", board[i][j], 0, 0)
+        game_play(board)
+    elsif (colour_input.downcase == "y")
+        update_board(board, :yellow, board[i][j], 0, 0)
+        game_play(board)
+    elsif (colour_input.downcase == "c")
+        update_board(board, :cyan, board[i][j], 0, 0)
+        game_play(board)
+    elsif (colour_input.downcase == "m")
+        update_board(board, :magenta, board[i][j], 0, 0)
+        game_play(board)
+    elsif (colour_input.downcase == "q")
+        main_menu()
+    else 
+        game_play(board)
+    end
+end
+
+def update_board(board, x, old, i, j)
+    
+    board[i][j] == x
+    if (board[i][j] != old)
+        return
+    end
+    update_board(board, x, old, i + 1, j)
+    update_board(board, x, old, i, j + 1)
+end
+    
+def completed_percent(board)
+    color_to_search = board[0][0]
+    num = 0
+    for i in 0...board.size
+      for j in 0...board[i].size
+          if (board[i][j] == color_to_search)
+              num += 1
+          end
+      end
+    end
+    total_blocks = $width_main * $height_main
+    return ((num * 100) / total_blocks)
+end
+
 def settings()
    
 end
